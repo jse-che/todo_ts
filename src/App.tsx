ba1cc2c7fbb14983fc408d15e29f33fe5a4612de
Jose Charris <jose.charris@horus-sc.com>
2024-04-30 14:26:34 -0500

    created list with id, title and completed, additionally import style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
-const App = () => {
+import { useState } from "react"
+import { Todos } from "./components/Todos"
+
+const mockTodos = [
+  {
+    id: '1',
+    title: 'todo 1',
+    completed: false,
+  },
+  {
+    id: '2',
+    title: 'todo 2',
+    completed: false,
+  },
+  {
+    id: '3',
+    title: 'todo 3',
+    completed: false,
+  }
+]
+
+const App = (): JSX.Element => {
+  const [todos] = useState(mockTodos)
+
   return (
-    <h1>todo mvc</h1>
+    <div className="todoapp">
+          <Todos todos={todos} />
+    </div>
   )
 }
 
